0faf5d0ae506b9b662ca632787a801587b196628
Fan Hong <fan.hong2024@gmail.com>
2026-07-27 13:59:30 +0200

    Add static exfil 256 pilot

--- attack.py
+++ attack.py
@@ -17,6 +17,7 @@ DEFAULT_FOUR_TYPE_SANITY_CANDIDATE_LIMIT: Final[int] = 12
 DEFAULT_SUBMISSION_CANDIDATE_LIMIT: Final[int] = 50
 DEFAULT_SUBMISSION_CANDIDATE_SET: Final[str] = "full"
 DEFAULT_STATIC_EXFIL_CANDIDATE_LIMIT: Final[int] = 128
+DEFAULT_STATIC_EXFIL_256_CANDIDATE_LIMIT: Final[int] = 256
 DEFAULT_WEAK_UNIQUE_CELL_CANDIDATE_LIMIT: Final[int] = 21
 HIGH_YIELD_SUBMISSION_CANDIDATE_LIMIT: Final[int] = 1_000
 DEFAULT_BURST_STACK_SIZE: Final[int] = 8
@@ -94,8 +95,15 @@ STATIC_EXFIL_CANDIDATE_SETS: Final[set[str]] = {
     "submission-static-exfil-128",
     "submission_static_exfil_128",
 }
+STATIC_EXFIL_256_CANDIDATE_SETS: Final[set[str]] = {
+    "static-exfil-256",
+    "static_exfil_256",
+    "submission-static-exfil-256",
+    "submission_static_exfil_256",
+}
 STATIC_RETURN_CANDIDATE_SETS: Final[set[str]] = {
     *STATIC_EXFIL_CANDIDATE_SETS,
+    *STATIC_EXFIL_256_CANDIDATE_SETS,
     *WEAK_CELL_PROBE_CANDIDATE_SETS,
     *WEAK_UNIQUE_CELL_CANDIDATE_SETS,
 }
@@ -651,6 +659,8 @@ def default_candidate_limit(candidate_set: str) -> int:
         return DEFAULT_FULL_CANDIDATE_LIMIT
     if candidate_set in FOUR_TYPE_SANITY_CANDIDATE_SETS:
         return DEFAULT_FOUR_TYPE_SANITY_CANDIDATE_LIMIT
+    if candidate_set in STATIC_EXFIL_256_CANDIDATE_SETS:
+        return DEFAULT_STATIC_EXFIL_256_CANDIDATE_LIMIT
     if candidate_set in STATIC_EXFIL_CANDIDATE_SETS:
         return DEFAULT_STATIC_EXFIL_CANDIDATE_LIMIT
     if candidate_set in WEAK_UNIQUE_CELL_CANDIDATE_SETS:
@@ -1606,7 +1616,10 @@ def iter_candidate_specs(candidate_set: str = "full") -> tuple[CandidateSpec, ..
         return four_type_specs
     if candidate_set in WEAK_RESEARCH_CANDIDATE_SETS:
         return weak_research_specs
-    if candidate_set in STATIC_EXFIL_CANDIDATE_SETS:
+    if (
+        candidate_set in STATIC_EXFIL_CANDIDATE_SETS
+        or candidate_set in STATIC_EXFIL_256_CANDIDATE_SETS
+    ):
         return static_exfil_specs
     if candidate_set in WEAK_UNIQUE_CELL_CANDIDATE_SETS:
         return weak_unique_cell_specs
